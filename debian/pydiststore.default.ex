1d597468721e1c9f0cca0b9763d5d0d80fe86aab
gray <gray@santeri.se>
2009-03-08 22:12:30 +0100

    add initial debian packaging

---
+++ debian/pydiststore.default.ex
@@ -0,0 +1,10 @@
+# Defaults for pydiststore initscript
+# sourced by /etc/init.d/pydiststore
+# installed at /etc/default/pydiststore by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
